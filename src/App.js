77dc0f2f73fdf7eb9a876fb43c1671b1858b78df
tr4allzz <tr4allzz@users.noreply.github.com>
2025-09-26 18:11:39 +0000

    deploy: 0408190190c9072d611d5e36e2dfde883694b496

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import HorseRacingApp from './components/HorseRacingApp';
+import './App.css';
+
+function App() {
+    return (
+        <div className="App">
+            <HorseRacingApp />
+        </div>
+    );
+}
+
+export default App;
